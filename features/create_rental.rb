87c6fa2809fa871e3711d40e738bee143e9f97f9
Metsanpeitto <abraham@waldenberginc.com>
2021-10-04 19:59:42 +0200

    Fix linters

---
+++ features/create_rental.rb
@@ -0,0 +1,17 @@
+require_relative '../texts'
+require_relative '../creators/rental'
+require_relative '../displayers/books'
+require_relative '../displayers/people'
+
+def feature_create_rental
+  Texts.text_cr1
+  list_books
+  book_index = gets.chomp
+
+  Texts.text_cr2
+  list_people
+  people_index = gets.chomp    
+  book = @books[book_index.to_i]
+  person = @people[people_index.to_i]
+  create_rental(book.title, person[:name])
+end
