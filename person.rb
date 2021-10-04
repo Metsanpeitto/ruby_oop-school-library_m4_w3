6fd50f756e4e450b0b0d316d9efc997437ba993c
Metsanpeitto <abraham@waldenberginc.com>
2021-10-04 23:07:22 +0200

    Fixed TSE suggestions

--- person.rb
+++ person.rb
@@ -9,6 +9,7 @@ class Person
     @name = parameters[:name] || 'Unknown'
     @age = parameters[:age]
     @parent_permission = parameters.fetch(:parent_permission, true)
+    @corrector = Corrector.new
   end
 
   def can_use_services?
@@ -16,8 +17,7 @@ class Person
   end
 
   def validate_name
-    corrector = Corrector.new
-    @name = corrector.correct_name @name
+    @name = @corrector.correct_name @name
   end
 
   private
