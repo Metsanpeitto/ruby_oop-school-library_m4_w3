924e9ad02bef91b4da7fd5e74924600e8822013f
Metsanpeitto <abraham@waldenberginc.com>
2021-10-04 09:35:44 +0200

    Add last test

--- RSpec/spec/teacher_spec.rb
+++ RSpec/spec/teacher_spec.rb
@@ -1,11 +1,22 @@
 require './classes/teacher'
-require './classes/person'
+require_relative './classes/person'
 
-describe Teacher do
+describe Teacher Person do
   context 'Given arguments to initialize the class' do
-    it 'create a teacher with specialization as argument ' do
-      teacher = Teacher.new('Science')
-      expect(teacher.play_hookey).to(eq("¯\(ツ)/¯"))
+    subject { described_class.new(teacher) }
+
+    let(:teacher) do
+      Teacher.new('Foo bar')
     end
+
+    it 'serializes object' do
+      expect(subject.person).to eq({
+                                     specialization: 'Foo bar'
+                                   })
+    end
+    # it 'create a teacher with specialization as argument ' do
+    #  teacher = Teacher.new('Science')
+    #  expect(teacher.play_hookey).to(eq("¯\(ツ)/¯"))
+    # end
   end
 end
