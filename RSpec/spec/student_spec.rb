c85792a13a4862ffb7c3b5424470c3eeb5e390a3
Metsanpeitto <abraham@waldenberginc.com>
2021-10-13 10:16:51 +0200

    Add little changes to the student class

--- RSpec/spec/student_spec.rb
+++ RSpec/spec/student_spec.rb
@@ -4,8 +4,8 @@ require './classes/person'
 describe Student do
   context 'Given arguments to initialize the class' do
     # it 'create a student' do
-    # student = Student.new('4F', Person)
-    it expect(subject.class.play_hookey).to(eq("¯\(ツ)/¯"))
+    p student = Student.new('4F', Person)
+    # it expect(student.play_hookey).to(eq("¯\(ツ)/¯"))
     # end
   end
 end
